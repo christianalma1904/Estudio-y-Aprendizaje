88aee61141600b1c5671ef6e446208a32e2961f6
Christian Alcocer <christianalma001@gmail.com>
2025-04-29 17:27:34 -0500

    Editado correctamente

--- Importante/map.js
+++ Importante/map.js
@@ -1,4 +1,5 @@
 //MAP
+//1.
 const nums = [1,2,3,4,5]
 
 const doble = nums.map(function(num){
@@ -7,3 +8,22 @@ const doble = nums.map(function(num){
 console.log(nums);
 console.log(doble);
 
+
+//2.
+const productos = [
+    {id: '001', nombre: 'camiseta', precio: 5},
+    {id: '002', nombre: 'zapatillas', precio: 20},
+    {id: '003', nombre: 'pantalon', precio: 15},
+    ];
+    
+const productosDescuento = productos.map(function(producto){
+    if(producto.precio < 10) return producto;
+    
+    return {
+        ...producto,
+        precio: producto.precio * 0.9
+    }
+});
+
+console.log(productos);
+console.log(productosDescuento);
