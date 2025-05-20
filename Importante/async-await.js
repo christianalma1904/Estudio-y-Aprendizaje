aa3303fabb32209f7a1e3dc53b404871afb9aeb2
Christian Alcocer <christianalma001@gmail.com>
2025-05-20 15:32:49 -0500

    Estudio editado correctamente

---
+++ Importante/async-await.js
@@ -0,0 +1,19 @@
+const datos = [{
+    id: 1,
+    title: 'Iron Man',
+    year: 2008
+},{
+    id: 2,
+    title: 'Spiderman: Homecoming',
+    year: 2017    
+},{
+    id: 3,
+    title: 'Avengers: End Game',
+    year: 2019
+}];
+
+const getDatos = () => {
+    return datos;
+}
+
+console.log(getDatos());
